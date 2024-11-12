c4b2148714206e3c17f818c2da13e060ea9f1c13
Vit Valek <vit.valek@nxp.com>
2024-11-12 12:20:41 +0100

    Fixed typo in license headers.

--- source/system/nt_system_frmstr_prv.h
+++ source/system/nt_system_frmstr_prv.h
@@ -1,5 +1,5 @@
 /*
-* Copyright 2013-2016, Freescale Semiconductor, Inc.
+* Copyright 2013-2016 Freescale Semiconductor, Inc.
 * Copyright 2016-2024 NXP
 *
 * NXP Proprietary. This software is owned or controlled by NXP and may
